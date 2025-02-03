9ff17ec748ecb5947caec2d79b801d02eed9060c
iamthemarkjack <rrohith2633@gmail.com>
2025-02-03 22:15:09 +0530

    first commit

---
+++ clenshaw/qroot_float_acc.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+#include<complex.h>
+
+void generate_alphas(double alpha_start, double alpha_end, int N, double *alpha){
+	double r = pow(alpha_end / alpha_start, 1.0/(N-1));
+	printf("Value of r: %lf\n ", r);
+	alpha[0] = alpha_start;
+	for (int i=1; i<N; i++){
+		alpha[i] = alpha[i-1]*r;
+	}
+}
+
+int sgn(double x)
+{
+	if(x>0) return 1;
+	if(x<0) return -1;
+	return 0;
+}
+
+int main(){
+	double alpha_start, alpha_end;
+	int N;
+
+	printf("Enter alpha_start: ");
+	scanf("%lf", &alpha_start);
+	printf("Enter alpha_end: ");
+	scanf("%lf", &alpha_end);
+	printf("Enter N: ");
+	scanf("%d", &N);
+
+	double *alpha = (double *)malloc(N*sizeof(double));
+	generate_alphas(alpha_start,alpha_end,N,alpha);
+
+	FILE *out = fopen("accurate_float.txt","w");
+	for (int i=0; i<N; i++){
+	        double complex root1, root2;
+		float  a = alpha[i];
+		if (a<1){
+		        root1 = -a + csqrt(a*a - 1);
+		        root2 = -a - csqrt(a*a - 1);
+		}else{
+		    root2 = -(a + sgn(a)*csqrt(a*a - 1));
+		    root1 = 1/root2;
+		}
+
+		fprintf(out, "%0.15lf,%0.15lf,%0.15lf,%0.15lf\n", creal(root1),cimag(root1),creal(root2),cimag(root2));
+	}
+
+	fclose(out);
+	free(alpha);
+	return 0;
+}
